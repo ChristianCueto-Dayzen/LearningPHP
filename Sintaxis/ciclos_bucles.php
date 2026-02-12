94daab32d62588c806b1e9a9c9642563b4eebde5
Christian Cueto <cdcueto@dayzen.mx>
2026-02-12 13:42:51 -0600

    ciclos_bucles: added

---
+++ Sintaxis/ciclos_bucles.php
@@ -0,0 +1,22 @@
+<?php
+/*
+//for
+for ($i = 1 ; $i<=10 ; $i++) {
+    //instrucciones
+    echo 'hola' . '<br />';
+}
+*/
+
+//while
+$numero = 0;
+while ($numero<5) {
+
+if ($numero < 5){
+    echo 'hola <br \>';
+}
+
+$numero++;
+}
+
+
+?>
