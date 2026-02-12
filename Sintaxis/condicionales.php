39cebea57cdc47d0d3473abe53804160a0abd645
Christian Cueto <cdcueto@dayzen.mx>
2026-02-12 12:51:31 -0600

    condicionales.php: added

---
+++ Sintaxis/condicionales.php
@@ -0,0 +1,50 @@
+<?php
+/*
+    if(condicion){
+        //instrucciones
+    }
+*/
+/*
+$edad = 17;
+$nombre = 'Christsan';
+if($edad >= 17 && $nombre == 'Christian'){
+    echo '<h1>Bienvenido</h1>';
+} 
+if($edad < 17 OR $nombre != 'Christian'){
+    echo '<h1>No puedes entrar</h1>';
+}
+*/
+
+/* #else
+
+$edad=17;
+
+if($edad >= 18){
+    echo 'Bienvenido';
+}else{
+    echo 'No eres bienvenido';
+}
+*/
+/* #elseif (condicional anidada)
+$mes = 'Diciembre';
+
+if($mes == 'Diciembre'){
+    echo 'Feliz Navidad';
+} elseif($mes == 'Enero'){
+    echo 'Feliz día de reyes';
+} elseif($mes == 'Mayo'){
+    echo 'Feliz cumpleaños';
+} else {
+    echo 'Nada por celebrar';
+}
+*/
+
+$boleano = true;
+
+if ($boleano) {
+    echo 'verdadero';
+} else {
+    echo 'falso';
+}
+
+?>
