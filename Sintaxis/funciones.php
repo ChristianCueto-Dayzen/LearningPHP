deb4d4944e7de2b0061527755f8acbf861029acc
Christian Cueto <cdcueto@dayzen.mx>
2026-02-12 16:16:49 -0600

    funciones.php: funcionalidad de php 7 para forzar un tipo de dato en el return

--- Sintaxis/funciones.php
+++ Sintaxis/funciones.php
@@ -16,4 +16,15 @@ $total = suma(1,2);
 
 echo $total . '<br />';
 
+//Nota: podemos forzar que devuelva un tipo de dato específico:
+//declare(strict_types=1); -> php es inteligente y puede forzar el cambio de string '1' a 1. Con esto evitas que lo haga.
+function sumar($a,$b): int{
+    $c =$a + $b;
+    return $c;
+}
+
+$total = sumar(1,2);
+
+echo $total . '<br />';
+
 ?>
