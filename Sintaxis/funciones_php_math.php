83102a152b2d56f65ca4c1114f2b9a8590c2af8d
Christian Cueto <cdcueto@dayzen.mx>
2026-02-12 15:48:44 -0600

    funciones_php_math.php: added

---
+++ Sintaxis/funciones_php_math.php
@@ -0,0 +1,21 @@
+<?php
+
+$numero=3.154331;
+
+echo round($numero,2).'<br />'; //redondear un número
+
+echo rand(1,10).'<br />'; //random entre un rango
+
+echo ceil(14.00001).'<br />'; //redondear hacia arriba siempre
+
+echo M_PI.'<br />'; //es una constante del numero pi, 13 dígitos
+echo M_2_PI.'<br />'; // 2/PI
+
+/* Podrias utilizar:
+
+
+
+    Más funciones:
+*/
+
+?>
