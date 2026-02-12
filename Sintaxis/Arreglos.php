1e7854acb36831caab4c1ccf05d098fda1068ef9
Christian Cueto <cdcueto@dayzen.mx>
2026-02-12 12:26:17 -0600

    Arreglos.php: updated

--- Sintaxis/Arreglos.php
+++ Sintaxis/Arreglos.php
@@ -1,5 +1,5 @@
 <?php
-
+/*
 echo 'normales' . '<br />';
 # normales
 $semana = array('Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo');
@@ -24,8 +24,63 @@ $amigos = array(
 );
 
 echo $amigos[2][0];
+*/
 
+$meses=array(
+    'Enero','Febrero','Marzo','Abril',
+    'Mayo','Junio','Julio','Agosto',
+    'Septiembre','Octubre', 'Noviembre', 'Diciembre'
+);
+//Alfabetico
+#sort($meses);
+//Reverso alfabetico
+rsort($meses);
+
+$numeros=array(1,62,2,6,179,257,35,10);
+
+rsort($numeros);
+
+/*
+echo 'Total de elementos en arreglo meses: ' . count($meses). '<br /><br />';
+
+$ultimo_mes= count($meses)-1;
+echo 'Ultimo mes: ' . $meses[$ultimo_mes];
+*/
 ?>
+
+<!DOCTYPE html>
+<html lang = "en">
+    <meta charset="UTF-8">
+    <title>Meses del año</title>
+</html>
+<body>
+    <h1>Meses del año</h1>
+    <ul>
+       <!--
+        <li>Enero</li>
+        <li>Febrero</li>
+        <li>Marzo</li>
+        <li>...</li>
+        -->
+        <!--
+        <li><?php echo $meses[0]?></li>
+        <li><?php echo $meses[1]?></li>
+        <li><?php echo $meses[2]?></li>
+        <li><?php echo $meses[3]?></li>
+        <li><?php echo '...'?></li>
+        -->
+        <?php 
+            /*bucle para recorrer $meses
+            foreach($meses as $mes) {
+                echo '<li>'.$mes.'</li>';
+            }*/
+
+            foreach($numeros as $numero){
+                echo '<li>'.$numero.'</li>';
+            }
+        ?>
+    </ul>
+</body>
 
 
 
